a3fe4a572fac947756f341297ddf48f3cb4cc795
RainbowEclipsee <dexterrullez1@gmail.com>
2024-03-05 23:18:16 +0300

    added filter to the EffectSection component

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect, useCallback } from 'react'
+import useInput from '../hooks/useInput.js'
 import Button from './Button/Button.jsx'
 import Modal from './Modal/Modal'
 
 export default function EffectSection() {
+  const input = useInput()
   const [modal, setModal] = useState(false)
-  
+  const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState([])
+
+  const fetchUsers = useCallback(async () => {
+    setLoading(true)
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await response.json()
+    setUsers(users)
+    setLoading(false)
+  }, [])
+
+  useEffect(() => {
+    fetchUsers()
+  }, [fetchUsers])
+
   function openModal() {
     setModal(true)
   }
@@ -13,7 +29,9 @@ export default function EffectSection() {
     <section>
       <h3>Effects</h3>
 
-      <Button onClick={openModal}>Открыть информацию</Button>
+      <Button style={{ marginBottom: '1rem' }} onClick={openModal}>
+        Открыть информацию
+      </Button>
 
       <Modal open={modal}>
         <h3>Modal</h3>
@@ -25,6 +43,23 @@ export default function EffectSection() {
         </p>
         <Button onClick={() => setModal(false)}>Close modal</Button>
       </Modal>
+
+      {loading && <p>Loading...</p>}
+
+      {!loading && (
+        <>
+          <input type="text" className="control" {...input} />
+          <ul>
+            {users
+              .filter((user) =>
+                user.name.toLowerCase().includes(input.value.toLowerCase())
+              )
+              .map((user) => (
+                <li key={user.id}>{user.name}</li>
+              ))}
+          </ul>
+        </>
+      )}
     </section>
   )
 }
